dcb14676fec1986e6f3e94b4350fe24438d5d9a2
marceaubilger <marceau.bilger@orange.fr>
2026-05-07 13:01:17 +0200

    Ajout ArtworksDaoImp

---
+++ src/main/java/com/project/artconnect/dao/impl/ArtworksDaoImpl.java
@@ -0,0 +1,34 @@
+package com.project.artconnect.dao.impl;
+import com.project.artconnect.model.Artist;
+import com.project.artconnect.dao.ArtworkDao;
+import com.project.artconnect.model.Artwork;
+import com.project.artconnect.service.impl.InMemoryArtistService;
+import com.project.artconnect.service.impl.InMemoryArtworkService;
+import java.util.List;
+import java.util.Optional;
+
+public class ArtworksDaoImpl implements ArtworkDao {
+    private final InMemoryArtworkService service = new InMemoryArtworkService();
+    private final InMemoryArtistService serviceArtists = new InMemoryArtistService();
+
+    public List<Artwork> findAll() {
+        return  service.getAllArtworks();
+    }
+
+    public void save(Artwork artwork) {
+        service.createArtwork(artwork);
+    }
+
+    public void update(Artwork artwork) {
+        service.updateArtwork(artwork);
+    }
+
+    public void delete(String artworkName) {
+        service.deleteArtwork(artworkName);
+    }
+
+    public List<Artwork> findByArtistName(String artistName) {
+        Artist a = serviceArtists.getArtistByName(artistName).orElse(null);
+        return service.getArtworksByArtist(a);
+    }
+}
