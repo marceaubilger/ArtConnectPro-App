b7eda99934fe848d893393e4559579756e66e259
Grégoire <puma-cattle-bunch@duck.com>
2026-05-07 12:11:08 +0200

    youpi

---
+++ src/main/java/com/project/artconnect/dao/impl/ArtistDaoImpl.java
@@ -0,0 +1,19 @@
+package com.project.artconnect.dao.impl;
+
+import com.project.artconnect.dao;
+
+public class ArtistDao implements ArtistDao {
+    List<Artist> findAll() {
+
+    }
+
+    void save(Artist artist) {
+
+    }
+
+    void update(Artist artist) {}
+
+    void delete(String artistName) {}
+
+    List<Artist> findByCity(String city) {}
+}
